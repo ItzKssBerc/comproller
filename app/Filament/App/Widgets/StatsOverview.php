2d731b68cd70176ebe2ba3e82df350d17ec72205
Kiss Bercel <kiss.berci20@gmail.com>
2026-02-27 12:12:23 +0100

    v1.5

--- app/Filament/App/Widgets/StatsOverview.php
+++ app/Filament/App/Widgets/StatsOverview.php
@@ -5,7 +5,7 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class StatsOverview extends StatsOverviewWidget
+class AppStatsOverview extends StatsOverviewWidget
 {
     protected function getStats(): array
     {
