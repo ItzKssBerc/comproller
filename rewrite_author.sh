47a014ee365d0900ef429dd8a8d9d67da8267ecd
Kiss Bercel <kiss.berci20@gmail.com>
2026-02-27 12:18:36 +0100

    Remove Git history rewriting scripts and the commits.txt file.

--- rewrite_author.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export GIT_AUTHOR_NAME="Kiss Bercel"
-export GIT_AUTHOR_EMAIL="kiss.berci20@gmail.com"
-export GIT_COMMITTER_NAME="Kiss Bercel"
-export GIT_COMMITTER_EMAIL="kiss.berci20@gmail.com"
--- rewrite_history.ps1
+++
@@ -1,9 +0,0 @@
-$hashes = git log --reverse --format=%H main
-git checkout --orphan clean-history
-foreach ($h in $hashes) {
-    git checkout $h -- .
-    $msg = git log -1 --format=%B $h
-    git add -A
-    git commit -m $msg --author="Kiss Bercel <kiss.berci20@gmail.com>"
-}
-git branch -M main
